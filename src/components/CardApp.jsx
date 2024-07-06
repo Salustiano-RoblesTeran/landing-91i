261d4da2325ee331ffbe25fdfa58697c77c43bbe
Salustiano-RoblesTeran <saluroblesteran@gmail.com>
2024-07-05 21:39:42 -0300

    Arregle color de las letras en el modo oscuro

--- src/components/CardApp.jsx
+++ src/components/CardApp.jsx
@@ -4,14 +4,14 @@ const CardApp = ({ item, modoOscuro }) => {
   return (
     <>
       <div className="col-12 col-md my-3">
-        <div className={`card h-100 ${modoOscuro?("text-bg-secondary"): ""}`}>
-          <div className= "card-body d-flex flex-column align-items-center">
+        <div className={`card h-100 ${modoOscuro ? "text-bg-secondary" : ""}`}>
+          <div className="card-body d-flex flex-column align-items-center">
             <i
               className={`fa ${item.icono} fa-3x text-primary`}
               aria-hidden="true"
             ></i>
-            <p className={`text-muted fs-3 fs-md-2 ${modoOscuro?("text-white") : ""}`}>{item.texto1}</p>
-            <p className={`text-muted ${modoOscuro?("text-white") : ""}`}>{item.texto2}</p>
+            <p className={`fs-3 fs-md-2 ${modoOscuro ? "text-white" : ""}`}>{item.texto1}</p>
+            <p className={`${modoOscuro ? "text-white" : ""}`}>{item.texto2}</p>
           </div>
         </div>
       </div>
